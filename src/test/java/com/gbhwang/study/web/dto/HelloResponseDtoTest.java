fccc9f05c57638c2d38521c8bfafe55046b187be
Hwang Geumbi <be_have98@naver.com>
2021-10-31 23:21:41 +0900

    JPA를 통한 게시글 관련 CRUD 및 게시글 저장 테스트 코드 구현

--- src/test/java/com/gbhwang/study/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/gbhwang/study/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,6 @@
 package com.gbhwang.study.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
