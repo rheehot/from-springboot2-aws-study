287fe3de9eb47716d402d8f4567a4fec9e557945
Hwang Geumbi <be_have98@naver.com>
2021-11-01 23:58:40 +0900

    게시글 수정 API 및 테스트 코드 추가

--- src/main/java/com/gbhwang/study/service/posts/PostsService.java
+++ src/main/java/com/gbhwang/study/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.gbhwang.study.service.posts;
 
+import com.gbhwang.study.domain.posts.Posts;
 import com.gbhwang.study.domain.posts.PostsRepository;
+import com.gbhwang.study.web.dto.PostsResponseDto;
 import com.gbhwang.study.web.dto.PostsSaveRequestDto;
+import com.gbhwang.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
