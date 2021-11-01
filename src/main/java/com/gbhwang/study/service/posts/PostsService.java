04db29488603966d6463dda513d5aee0f8d025a7
Hwang Geumbi <be_have98@naver.com>
2021-11-01 23:31:43 +0900

    게시글 저장 API 및 테스트 코드 추가

---
+++ src/main/java/com/gbhwang/study/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.gbhwang.study.service.posts;
+
+import com.gbhwang.study.domain.posts.PostsRepository;
+import com.gbhwang.study.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
